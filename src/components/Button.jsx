0acde33c06576dcd7d4e298e921d131f10dae46c
InevitableDesire <emanuelamicoo@gmail.com>
2021-07-27 20:20:24 -0300

    fix link target

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import arrowIcon from "../assets/static/icons/arrow-icon.svg";
 import "../assets/styles/components/Button.scss";
 
-const Button = ({ text, href }) => {
+const Button = ({ text, href, target }) => {
   return (
-    <a href={href} target="_blank" className="button-link">
+    <a href={href} target={target ?? "_blank"} className="button-link">
       <button>
         {text}
         <img src={arrowIcon} alt="arrow icon" className="arrow" />
