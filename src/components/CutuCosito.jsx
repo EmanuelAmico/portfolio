94a05be50c61a536dd6dc45148df613b78754f53
InevitableDesire <emanuelamicoo@gmail.com>
2021-07-31 03:49:54 -0300

    background animation

---
+++ src/components/CutuCosito.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const CutuCosito = () => {
+  return (
+    <div class="view">
+        <div class="plane main">
+          <div class="circle"></div>
+          <div class="circle"></div>
+          <div class="circle"></div>
+          <div class="circle"></div>
+          <div class="circle"></div>
+          <div class="circle"></div>
+        </div>
+      </div>
+  )
+}
+
+export default CutuCosito
