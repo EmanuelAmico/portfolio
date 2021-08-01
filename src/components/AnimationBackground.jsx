56bd40b4f09aa077cbb6008359f3b95ae5300108
InevitableDesire <emanuelamicoo@gmail.com>
2021-08-01 03:56:20 -0300

    refactoring styles and headings

--- src/components/AnimationBackground.jsx
+++ src/components/AnimationBackground.jsx
@@ -3,14 +3,14 @@ import '../assets/styles/components/AnimationBackground.scss'
 
 const AnimationBackground = () => {
   return (
-    <div class="view">
-        <div class="plane main">
-          <div class="circle"></div>
-          <div class="circle"></div>
-          <div class="circle"></div>
-          <div class="circle"></div>
-          <div class="circle"></div>
-          <div class="circle"></div>
+    <div className="view">
+        <div className="plane main">
+          <div className="circle"></div>
+          <div className="circle"></div>
+          <div className="circle"></div>
+          <div className="circle"></div>
+          <div className="circle"></div>
+          <div className="circle"></div>
         </div>
       </div>
   )
